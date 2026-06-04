1e197778bce8ee7c4bc3c15cf0b786484af945d8
Tindra <grundbergtindra@gmail.com>
2026-06-04 12:23:23 +0200

    Fix syntax error in UseStatusCodePagesWithReExecute

--- TaskFlow.Blazor/Program.cs
+++ TaskFlow.Blazor/Program.cs
@@ -22,7 +22,7 @@ if (!app.Environment.IsDevelopment())
     
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStatusCodePagesWithReExecute("/not-found";
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
